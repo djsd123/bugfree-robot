5b6d217caff9174f6723865a6f5a012ee57a77ce
Mikael <mikellallison@gmail.com>
2015-06-22 21:57:29 +0100

    Python3 setup untested

--- modules/profiles/manifests/python_tools.pp
+++ modules/profiles/manifests/python_tools.pp
@@ -1,5 +1,37 @@
 class profiles::python_tools {
 
+  require profiles::packages
+# Python 3 setup\install
+
+  $PYTHON   = 'lr-python3-3.4.3-1.x86_64'
+  $PYPGK    = "${PYTHON}.rpm"
+  $PKGMAN   = 'rpm'
+
+  package {
+    'python-pip':
+      ensure      => installed,
+      provider    => yum,
+  }
+
+  file {'LR Python package':
+    ensure => 'file',
+    path   => "/tmp/${PYPGK}",
+    source => "puppet:///modules/profiles/${PYPGK}"
+  }
+
+# Install custom Python 3.4.3 build
+  package { $PYTHON :
+    ensure   => installed,
+    provider => $PKGMAN,
+    source   => "/tmp/${PYPGK}",
+    require  => File['LR Python package']
+  }
+
+  file {'/usr/bin/pip3' :
+    ensure => link,
+    target => '/usr/local/bin/pip3',
+  }
+
 
 
 }
