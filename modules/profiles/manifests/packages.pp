b7f2c47588a2716f028aa70c7837b763c43eb1d6
Mikael <mikellallison@gmail.com>
2015-06-22 21:56:57 +0100

    separating the python setup from packages

--- modules/profiles/manifests/packages.pp
+++ modules/profiles/manifests/packages.pp
@@ -9,37 +9,9 @@ class profiles::packages {
    'binutils','bison','flex','gettext','libtool','make','patch','pkgconfig',
    'redhat-rpm-config','rpm-build','rpm-sign','libxml2','libxslt',
    'libxml2-devel','psmisc']
- $PYTHON   = 'lr-python3-3.4.3-1.x86_64'
- $PYPGK    = "${PYTHON}.rpm"
- $PKGMAN   = 'rpm'
+
 
 
  ensure_packages($PKGLIST)
-
-  package {
-    'python-pip':
-      ensure      => installed,
-      provider    => yum,
-      require     => Package[$PKGLIST];
-  }
-
-  file {'LR Python package':
-    ensure => 'file',
-    path   => "/tmp/${PYPGK}",
-    source => "puppet:///modules/profiles/${PYPGK}"
-  }
-
-# Install custom Python 3.4.3 build
-  package { $PYTHON :
-    ensure   => installed,
-    provider => $PKGMAN,
-    source   => "/tmp/${PYPGK}",
-    require  => File['LR Python package']
-  }
-
-  file {'/usr/bin/pip3' :
-    ensure => link,
-    target => '/usr/local/bin/pip3',
-  }
 
 }
