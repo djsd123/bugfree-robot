cf57ca5c92cf4ed1e2f9df81c0b5dff82f115d5e
Mikael <mikellallison@gmail.com>
2015-06-24 21:54:30 +0100

    This is the nginx configuration profile.  It uses the Forge module to install nginx.  It drops files to where they need to be and adds things like server-name to reverse proxy config.

--- modules/profiles/manifests/reverse_proxy.pp
+++ modules/profiles/manifests/reverse_proxy.pp
@@ -1,13 +1,27 @@
 class profiles::reverse_proxy {
 
-  class {'nginx':}
+  require ::nginx
+
+  $server_name  =  hiera('server_name')
+  $sslcrt       =  hiera('sslcrt')
+  $sslkey       =  hiera('sslkey')
+  $proxy_pass   =  hiera('proxy_pass')
+
 
   file {'/etc/nginx/conf.d/reverse_proxy.conf':
     ensure  => file,
-    content => epp('profiles/proxy.conf.epp'),
-    owner   =>
-    group   =>
+    content => template('profiles/proxy.conf.erb'),
+    owner   => 'nginx',
+    group   => 'nginx',
+  }
 
+  file {['$sslcrt','$sslkey']:
+    ensure  => file,
+    source  => ["puppet:///profiles/ssl.crt",
+                "puppet:///profiles/ssl.key"
+    ],
+    owner   => 'nginx',
+    group   => 'nginx',
 
   }
 
