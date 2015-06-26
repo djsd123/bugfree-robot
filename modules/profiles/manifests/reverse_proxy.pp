b0506a8dc0712d80fbae7c71c6cdad055b46dba6
Mikael <mikellallison@gmail.com>
2015-06-26 11:03:34 +0100

    Installs nginx, sources ssl keys\certs, copies a template for reverse proxying and starts the service.  Had to get ordering right to enable this.

--- modules/profiles/manifests/reverse_proxy.pp
+++ modules/profiles/manifests/reverse_proxy.pp
@@ -1,28 +1,39 @@
 class profiles::reverse_proxy {
 
-  require ::nginx
+  require profiles::packages
 
   $server_name  =  hiera('server_name')
   $sslcrt       =  hiera('sslcrt')
   $sslkey       =  hiera('sslkey')
   $proxy_pass   =  hiera('proxy_pass')
 
-
-  file {'/etc/nginx/conf.d/reverse_proxy.conf':
+  package { 'nginx':
+    ensure  => installed,
+  } ->
+  file { '/etc/nginx/conf.d/reverse_proxy.conf':
     ensure  => file,
     content => template('profiles/proxy.conf.erb'),
     owner   => 'nginx',
     group   => 'nginx',
-  }
+    require => Package['nginx'],
+  } ~>
 
-  file {['$sslcrt','$sslkey']:
+  file { $sslkey :
     ensure  => file,
-    source  => ["puppet:///profiles/ssl.crt",
-                "puppet:///profiles/ssl.key"
-    ],
+    source  => "puppet:///modules/profiles/ssl.pem",
+    owner   => 'root',
+    group   => 'root',
+    require => Package['nginx'],
+  } ~>
+  file { $sslcrt :
+    ensure  => file,
+    source  => "puppet:///modules/profiles/ssl1.pem",
     owner   => 'nginx',
     group   => 'nginx',
-
+  } ~>
+  service { 'nginx':
+    ensure    => running,
+    subscribe => File['/etc/nginx/conf.d/reverse_proxy.conf'],
   }
 
 }
