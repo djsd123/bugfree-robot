5a2831044367350157e49f1a385d0f056f996aca
Mikael <mikellallison@gmail.com>
2015-06-26 16:25:34 +0100

    Initial commit for logstash-forwarder profile.

--- modules/profiles/manifests/logstash_forwarder.pp
+++ modules/profiles/manifests/logstash_forwarder.pp
@@ -1,10 +1,21 @@
 class profiles::logstash_forwarder {
 
-  class {'logstashforwarder':
+  $logstash_file = hiera('logstash_file')
+  $app_log       = hiera('app_log')
+  $app           = hiera('app')
+  $servers       = hiera('servers')
+  $ssl_ca        = hiera('ssl_ca')
+
+  class { 'logstashforwarder':
     manage_repo => true,
     servers     => $servers,
     timeout     => '15',
     ssl_ca      => $ssl_ca,
+  }
+
+  logstashforwarder::file { $logstash_file :
+    paths  => [ "/var/log/applications/$app_log" ],
+    fields => { 'type' => $app },
   }
 
 }
