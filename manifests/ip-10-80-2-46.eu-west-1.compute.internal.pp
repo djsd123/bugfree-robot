27d95eca3224c565bed78b3bfd9929cd775ad9ef
Mikael <mikellallison@gmail.com>
2015-06-27 01:27:02 +0100

    Added elasticsearch updater logs

--- manifests/ip-10-80-2-46.eu-west-1.compute.internal.pp
+++ manifests/ip-10-80-2-46.eu-west-1.compute.internal.pp
@@ -2,4 +2,9 @@
 node 'ip-10-80-2-46.eu-west-1.compute.internal' {
   include '::roles::aws'
   include '::profiles::search'
+
+  logstashforwarder::file { 'digital-register-elasticsearch-updater':
+    paths  => [ '/var/log/applications/digital-register-elasticsearch-updater.log' ],
+    fields => { 'type' => 'digital-register-elasticsearch-updater' },
+  }
 }
