1a55ed1c63cb5f561b96022912a33c1abb2fb3b2
Mikael <mikellallison@gmail.com>
2015-06-27 01:26:26 +0100

    Tidying up manifest

--- manifests/ip-10-80-1-91.eu-west-1.compute.internal.pp
+++ manifests/ip-10-80-1-91.eu-west-1.compute.internal.pp
@@ -5,6 +5,6 @@ node 'ip-10-80-1-91.eu-west-1.compute.internal' {
 
   logstashforwarder::file { 'nginx':
     paths  => [ '/var/log/nginx/access.log' ],
-    fields=> { 'type' => 'nginx-access' },
+    fields => { 'type' => 'nginx-access' },
   }
 }
