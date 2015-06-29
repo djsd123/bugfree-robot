8b0fd55dbbd30f392c9f2659d94f92298621b3cb
Mikael <mikellallison@gmail.com>
2015-06-29 14:51:51 +0100

    rule requires create_mode attribute when using create owner

--- modules/profiles/manifests/log_rotation.pp
+++ modules/profiles/manifests/log_rotation.pp
@@ -2,7 +2,7 @@ class profiles::log_rotation {
 
   $app    = hiera('app')
 
-  logrotate::rule { $app:
+  logrotate::rule { $app :
     path         => "/var/log/applications/${app}.log",
     rotate       => 10,
     rotate_every => 'day',
@@ -10,6 +10,7 @@ class profiles::log_rotation {
     create       => true,
     create_owner => 'webapp',
     create_group => 'webapp',
+    create_mode  => 755,
     compress     => true,
   }
 
