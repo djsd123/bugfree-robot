79f55cd105ccc400e718fc9ac66ca11e2fcd469f
Mikael <mikellallison@gmail.com>
2015-07-07 22:40:09 +0100

    Fixing variable

--- modules/profiles/manifests/python_tools2.pp
+++ modules/profiles/manifests/python_tools2.pp
@@ -3,7 +3,7 @@ class profiles::python_tools2 {
 # Very ugly approach, I know.  I am experimenting with using python packages system wide as opposed to virtualenvs
 # which are not relocatable
 
-  $rpm_path   =  "puppet:///modules/profiles/files"
+  $rpm_path   =  "puppet:///modules/profiles"
   $gunicorn   =  "python3-gunicorn-18.0-1.fc20.noarch.rpm"
   $python3    =  "python3-3.3.2-19.fc20.x86_64.rpm"
   $pip        =  "python3-pip-1.4.1-1.fc20.noarch.rpm"
@@ -14,7 +14,7 @@ class profiles::python_tools2 {
     ensure   => file,
     path     => "/tmp/${setuptools}",
     source   => "${rpm_path}/${setuptools}",
-  } ->
+  } ~>
 
   package { $setuptools :
     provider => rpm,
@@ -26,7 +26,7 @@ class profiles::python_tools2 {
     ensure   => file,
     path     => "/tmp/${libs}",
     source   => "${rpm_path}/${libs}",
-  } ->
+  } ~>
 
   package { $libs :
     provider => rpm,
@@ -38,7 +38,7 @@ class profiles::python_tools2 {
     ensure   => file,
     path     => "/tmp/${python3}",
     source   => "${rpm_path}/${python3}",
-  } ->
+  } ~>
 
   package { $python3 :
     provider => rpm,
@@ -51,7 +51,7 @@ class profiles::python_tools2 {
     ensure   => file,
     path     => "/tmp/${pip}",
     source   => "${rpm_path}/${pip}",
-  } ->
+  } ~>
 
   package { $pip :
     provider => rpm,
@@ -64,7 +64,7 @@ class profiles::python_tools2 {
     ensure   => file,
     path     => "/tmp/${gunicorn}",
     source   => "${rpm_path}/${gunicorn}",
-  } ->
+  } ~>
 
   package { $gunicorn :
     provider => rpm,
