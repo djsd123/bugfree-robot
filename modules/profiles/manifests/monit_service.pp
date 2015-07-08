60d397a14e9afe378690bb27f7849447c5cb6878
Mikael <mikellallison@gmail.com>
2015-07-08 11:08:03 +0100

    Modifying monit start command and allowing webapp to sudo pip install

--- modules/profiles/manifests/monit_service.pp
+++ modules/profiles/manifests/monit_service.pp
@@ -4,7 +4,7 @@ class profiles::monit_service{
 
   monit::check { 'gunicorn':
     content => 'check process gunicorn with pidfile /tmp/gunicorn.pid
-    start program = "/var/webapp/env_up"
+    start program = "/usr/bin/python3-gunicorn service.server:app --pid=/tmp/gunicorn.pid -c gunicorn_settings.py &"
     as uid webapp and gid webapp
     stop program = "/bin/pkill -9 gunicron"',
   }
