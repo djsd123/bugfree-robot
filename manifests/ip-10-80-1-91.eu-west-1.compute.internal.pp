f6f8e9583e3d2dba7fe1397f3c3ae3684b93ee63
Mikael <mikellallison@gmail.com>
2015-07-13 13:59:04 +0100

    testing drv-int_variables profile on frontend node

--- manifests/ip-10-80-1-91.eu-west-1.compute.internal.pp
+++ manifests/ip-10-80-1-91.eu-west-1.compute.internal.pp
@@ -2,6 +2,7 @@
 node 'ip-10-80-1-91.eu-west-1.compute.internal' {
   include '::roles::aws'
   include '::profiles::reverse_proxy'
+  include '::profiles::drv_int_variables'
 
   logstashforwarder::file { 'nginx':
     paths  => [ '/var/log/nginx/access.log' ],
