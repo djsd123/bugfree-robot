651d3cacaa3151da7ee496a7cc0ba1326251c5ef
Mikael <mikellallison@gmail.com>
2015-07-15 11:56:57 +0100

    Taking a different approach.  Bringing back the env_up.  This creates a shell script that creates a virtualenv and runs the app.

---
+++ modules/profiles/manifests/drvapp.pp
@@ -0,0 +1,12 @@
+class profiles::drvapp {
+# The implemetation of this definitely needs to be revisted and aligned with a proper packaging and deployment solution
+  $app    = hiera('app')
+
+  file { '/var/webapp/env_up.sh':
+    ensure  => file,
+    content => template('profiles/env_up.sh.erb'),
+    owner   => 'webapp',
+    group   => 'webapp',
+  }
+
+}
