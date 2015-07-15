7ff7894eb6656851671cdc02f2a59416c08db080
Mikael <mikellallison@gmail.com>
2015-07-15 12:10:10 +0100

    Putting required profile class in for good measure as this profile requires the webapp user and it's home directory to exist.

--- modules/profiles/manifests/drvapp.pp
+++ modules/profiles/manifests/drvapp.pp
@@ -1,5 +1,6 @@
 class profiles::drvapp {
 # The implemetation of this definitely needs to be revisted and aligned with a proper packaging and deployment solution
+  require profiles::users
   $app    = hiera('app')
 
   file { '/var/webapp/env_up.sh':
@@ -7,6 +8,7 @@ class profiles::drvapp {
     content => template('profiles/env_up.sh.erb'),
     owner   => 'webapp',
     group   => 'webapp',
+    mode    => 755,
   }
 
 }
